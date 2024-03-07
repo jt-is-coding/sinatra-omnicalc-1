b736bb58d6f5240b8c2259b610f860b1604a88d7
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-07 17:43:18 +0000

    Initial setup

--- app.rb
+++ app.rb
@@ -2,8 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:square)
+
+end
+
+get("/square/new") do
+
+  erb(:square)
+
+end
+
+get("/square_root/new") do
+
+  erb(:square_root)
+
+end
+
+get("/payment/new") do
+
+  erb(:payment)
+
+end
+
+get("/random/new") do
+
+  erb(:random)
+
 end
